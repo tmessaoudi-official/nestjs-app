e411d5e90a5e15afc995184bad00e98a78e2398e
Takieddine Messaoudi <takieddine.messaoudi.official@gmail.com>
2020-12-12 21:17:13 +0000

    fix: jest tests and e2e

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,14 @@
-import { Controller, Get, Render, Req, Res } from '@nestjs/common';
+import { Controller, Get, Render, Req, Res, Inject } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { AppService } from './app.service';
 
 @Controller({ path: '/' })
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(@Inject(AppService) private readonly appService: AppService) {}
 
   @Get('')
   @Render('index.html.twig')
-  getHello(@Req() request: Request, @Res() response: Response): unknown {
-    return { title: 'Hello world!' };
+  invoke(@Req() request: Request): unknown {
+    return { title: 'Hello world! from : ' + request.ip };
   }
 }
