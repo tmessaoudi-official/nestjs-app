ef8f038393828a63a88f40bc646f5f92345712a4
Takieddine Messaoudi <takieddine.messaoudi.official@gmail.com>
2020-12-12 19:54:41 +0100

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Request as RequestDecorator } from '@nestjs/common';
+import { Request as RequestExpress } from 'express';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(@RequestDecorator() request: RequestExpress): string {
+    return (
+      this.appService.getHello() + ' => ' + JSON.stringify(request.headers)
+    );
+  }
+}
