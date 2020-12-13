d7ba796ebcad390b04b305e92a19d524e7c18493
Takieddine Messaoudi <takieddine.messaoudi.official@gmail.com>
2020-12-13 14:45:09 +0000

    feat: added app environments

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,24 @@
 import { Controller, Get, Render, Req, Res, Inject } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller({ path: '/' })
 export class AppController {
-  constructor(@Inject(AppService) private readonly appService: AppService) {}
+  constructor(
+    @Inject(AppService) private readonly appService: AppService,
+    @Inject(ConfigService) private readonly configService: ConfigService,
+  ) {}
 
   @Get('')
   @Render('index.html.twig')
   invoke(@Req() request: Request): unknown {
-    return { title: 'Hello world! from : ' + request.ip };
+    return {
+      title:
+        'Hello world! from : ' +
+        request.ip +
+        ' -- ' +
+        this.configService.get<string>('APP_ENV'),
+    };
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+process.env.NODE_DEBUG_FORCE = 'false';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const dotEnvLoader = require('@tmessaoudi-official/dot-env-loader/dot-env-loader.run.js').default(
+  'process',
+);
+
+const APP_ENV = dotEnvLoader.run();
+
+console.log(APP_ENV);
+console.log(APP_ENV.APP_ENV);
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      ignoreEnvFile: true,
+      isGlobal: true,
+      load: [() => APP_ENV],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
